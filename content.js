c594d606abd2421e72d8bf97c2aa3b6b240bf2ca
mwojcikbs <mwojcikbs@gmail.com>
2022-01-08 14:39:55 +0100

    second try

--- content.js
+++ content.js
@@ -1,9 +1,6 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 
-    alert(message.type);
-    console.log(message.type);
-
     if(message.type === 'popup-copy') {
-        alert(2344324);
+        sendResponse({farewell: "goodbye"});
     }
 });
